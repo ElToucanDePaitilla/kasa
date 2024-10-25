5287f47e2d9980ebd6e7f0951dfa6a5c83037411
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-10-25 16:43:02 +0200

    Style nav ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/components/_header.scss';
 import LogoHeaderBig from '../assets/images/logos/LogoHeaderBig.png';
 
 const Header = () => {
-    return (       
+    // État pour suivre l'onglet actif
+    const [activeTab, setActiveTab] = useState('');
+
+    // Définir l'onglet actif en fonction de l'URL actuelle
+    useEffect(() => {
+        const currentPath = window.location.pathname;
+        if (currentPath === '/') {
+            setActiveTab('accueil');
+        } else if (currentPath === '/about') {
+            setActiveTab('apropos');
+        }
+    }, []);
+
+    return (
         <div className="header">
-                <div className="logo-header">
-                    <a href="/"><img src={LogoHeaderBig} alt="Kasa Logo"/></a>
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="/about">A Propos</a></li>
-                    </ul>
-                </nav>
-        
-         </div>  
+            <div className="logo-header">
+                <a href="/"><img src={LogoHeaderBig} alt="Kasa Logo" /></a>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <a
+                            href="/"
+                            className={`nav-link ${activeTab === 'accueil' ? 'active' : ''}`}
+                        >
+                            Accueil
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="/about"
+                            className={`nav-link ${activeTab === 'apropos' ? 'active' : ''}`}
+                        >
+                            A Propos
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 };
 
