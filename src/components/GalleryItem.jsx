0a3b6e1c2cc47a44eede6f51fc68ade691a4264d
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-10-26 14:20:03 +0200

    galleryItem bug style résolu.

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -4,19 +4,18 @@ import { Link } from "react-router-dom";
 const GalleryItem = (props) => {
   return (
     <Link to={`/rental/${props.bien.id}`} className="gallery-item">
-      <div key={props.bien.id} >
-        <img
-          src={props.bien.cover}
-          alt={props.bien.title}
-          
-        />
+      <div key={props.bien.id}>
+          <img
+            src={props.bien.cover}
+            alt={props.bien.title}
+          />
 
-        {/* Overlay dégradé */}
-        <div className="gradient-overlay"></div>
+          {/* Overlay dégradé */}
+          <div className="gradient-overlay"></div>
 
-        <div className="gallery-item-title-overlay">
-          <h3>{props.bien.title}</h3>
-        </div>
+          <div className="gallery-item-title-overlay">
+            <h3>{props.bien.title}</h3>
+          </div>
       </div>
     </Link>
   );
