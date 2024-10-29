2575393566a08a28088b38ef118b66bbf2f204bd
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-10-29 12:10:09 +0100

    Home et 404 responsive finalisé

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Img404 from '../assets/images/error404/Img404.png';
 
 const Error404 = () => {
   return (
     <div className="msg404">
-      <img src={Img404} alt="Erreur 404" />
+      <p className="NotFound404">404</p>
       <p>Oups! La page que vous demandez n'existe pas.</p>
       <a href="/">Retourner sur la page d'accueil</a>
     </div>
