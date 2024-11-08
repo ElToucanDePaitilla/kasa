e444dede355ddfeb36f7223d09f1ee71d2a5cb28
Jean-Luc <eltoucandepaitilla@gmail.com>
2024-11-08 20:07:07 +0100

    Polissage du code.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,13 +4,11 @@ import rentalData from "../datas/rentals-list.json";
 
 const Gallery = () => {
   return (
-    <div className="gallery-container">
       <div className="gallery">
         {rentalData.map((bien) => (
           <GaleryItem key={bien.id} bien={bien} />
         ))}
       </div>
-    </div>
   );
 };
 
